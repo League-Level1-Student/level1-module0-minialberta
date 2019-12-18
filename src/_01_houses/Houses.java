0f0264858801a0c5d98e69e41497c1b6c630598b
League Student <league@iMac-23.attlocal.net>
2019-12-17 20:01:16 -0800

    Level 1 Module 0
    Level 1 Module 0
    Week 13

--- src/_01_houses/Houses.java
+++ src/_01_houses/Houses.java
@@ -1,7 +1,59 @@
 package _01_houses;
 
+import java.awt.Color;
+
+import org.jointheleague.graphical.robot.Robot;
+
 public class Houses {
-	public void run() {
+	 Robot rob=new Robot();
+	 
+	public static void main(String[] args) {
+	Houses obj=new Houses();
+		obj.house("small", "ff");
+	
 		
 	}
-}
+	void house(String height, String color) {
+		int x;
+		
+	if (height.equals("small")) {
+		x=60;
+		
+	}else if(height.equals("large")) {
+		x=250;
+		
+	}else {
+		x=120;
+	}
+		
+		for (int i = 0; i < 9; i++) {
+		rob.penDown();
+		rob.setSpeed(10);
+		
+		if (color.equals("Red")) {
+			rob.setPenColor(Color.red);
+		}else if (color.equals("Blue")) {
+			rob.setPenColor(Color.blue);
+		}else {
+			rob.setPenColor(Color.pink);
+		}
+		
+		rob.move(x);
+		rob.turn(90);
+		rob.move(25);
+		rob.turn(90);
+		rob.move(x);
+		rob.turn(-90);
+		rob.setPenColor(Color.green);
+		rob.move(25);
+		rob.turn(-90);
+	}	
+		
+		
+		
+	}
+
+	
+	public void run(String height, String color) {	
+	
+	}}
