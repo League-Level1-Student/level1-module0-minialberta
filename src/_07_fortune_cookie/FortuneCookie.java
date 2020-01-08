3a5f3c2c5b2deef8231a8f19cb8f0525707f3c38
League Student <league@iMac-14.attlocal.net>
2020-01-07 19:58:06 -0800

    Level 1 Module 1 x2
    2 more things

--- src/_07_fortune_cookie/FortuneCookie.java
+++ src/_07_fortune_cookie/FortuneCookie.java
@@ -1,5 +1,23 @@
 package _07_fortune_cookie;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
 public class FortuneCookie {
+	
+public void showButton() {
+	JFrame frame=new JFrame();
+    frame.setVisible(true);
+	            System.out.println("Button clicked");
+	            
+	       }
+	   
+public static void main(String[] args) {
+	JFrame frame=new JFrame();
+    JButton button = new JButton();
+    frame.add(button);
+    frame.pack();
+    
+}
 
 }
